a579016f0b47a8e6be1ef8fb206a616e2205f374
Leason <leason99>
2017-11-01 20:33:25 +0800

    1ch detect

--- setup.py
+++
@@ -1,9 +0,0 @@
-
-from distutils.core import setup, Extension
- 
-module1 = Extension('mcp3008', sources = ['mcp3008.c',"lfq.c"])
- 
-setup (name = 'mcp3008',
-       version = '1.0',
-       description = 'This is a mcp3008 package',
-       ext_modules = [module1])
